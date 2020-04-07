09251406f89a799bb6e7ea700a1886e42cd5d677
acvt499 <61745534+acvt499@users.noreply.github.com>
2020-04-07 17:07:36 +0100

    Segmenting all electrodes

--- Lexi_device_labeling_code.m
+++ Lexi_device_labeling_code.m
@@ -70,6 +70,7 @@ colormap(jet2)
 % ---------- Removing two objects that are not the elxtrodes ------------ %
 % 3389 and 1009 are the volume of two objects identified as noise in this
 % class
+% change for .... .Volume==props{index,1})
 electrodes_removed_1 = ismember(electrodes_labeled,...
     find(electrodes_labeled_props.Volume~=3389));
 electrodes_removed_1=bwlabeln(electrodes_removed_1);
@@ -87,86 +88,155 @@ imagesc(max(electrodes_opened_labeled(:,:,:),[],3))
 colormap(jet2)
 electrodes_open_labeled_props=regionprops3(electrodes_opened_labeled,...
     'Volume');
-% electrodes_open_labeled contains 56/62 electrodes
+% electrodes_open_labeled contains 55/62 electrodes
 %% --------------------------------------------------------------------- %%
 clear strel electrodes_labeled electrodes_labeled_props...
     electrodes_opened electrodes_props_removed_1 electrodes_removed_1...
     electrodes_removed_2
-
-%% --------------------- Opening the electrodes ------------------------ %%
-Expe=Set_2_labeled_without_small_noise;
-se = strel('line',8,180);
-Set_2_opening_1=imopen((Expe(370:420,85:140,:)),se);
-for i=370:420
-    for j=85:140
-Expe(i,j,:)=Set_2_opening_1(i-370+1,j-85+1,:);
-    end
-end
-imagesc(max(Expe(:,:,:),[],3))
-se=strel('rectangle',[2 8]);
-Set_2_opening_1=imopen((Expe),se);
-%%
-Expe_2=Expe;
-se = strel('line',12,30);
-Set_2_opening_2=imopen((Expe_2(276:295,85:110,:)),se);
-for i=276:295
-    for j=85:110
-Expe_2(i,j,:)=Set_2_opening_2(i-276+1,j-85+1,:);
-    end
-end
-%%
-se=strel('rectangle',[2 8]);
-Expe_3=imopen(Expe_2, se);
-imagesc(max(Expe_3(:,:,:),[],3))
-% se=strel('line',10,30);
-% Set_2_opening=imopen((Set_2_opening),se);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% figure
-% imagesc(max(Set_2_opening(:,:,:),[],3))
-% colormap(jet2)
-% Set_2_labeled_after_opening=bwlabeln(Set_2_opening);
-% Volume_labeled_open=regionprops3(Set_2_labeled_after_opening,'Volume');
+%% ----------- Finding missing electrodes in wires class --------------- %%
+%% ------------------------- Labeling wires ---------------------------- %%
+wires_labeled=bwlabeln(wires);
+imagesc(max(wires_labeled(:,:,:),[],3))
+wires_props=regionprops3(wires_labeled,'Volume');
+% --------------- Finding the electrodes by inspection ------------------ %
+figure
+subplot(1,2,1)
+imagesc(max(wires_labeled(:,:,:),[],3))
+subplot(1,2,2)
+imagesc(max(electrodes_opened_labeled(:,:,:),[],3))
+% ---------- There are 7 missing electrodes at this point -------------- %%
+%% ------------- Extracting missing electrodes 1 and 2 ----------------- %%
+% ------- Extracting Volume in which the elctrodes are "hiden" --------- %%
+Volume_electrodes_1_and_2=ismember(wires_labeled,...
+    find(wires_props.Volume==wires_props{1,1}));
+imagesc(max(Volume_electrodes_1_and_2(:,:,:),[],3))
+strel=strel('rectangle',[2,6]);
+Electrodes_1_and_2_with_noise=imopen(Volume_electrodes_1_and_2,strel);
+clear strel
+Electrodes_1_and_2_with_noise_labeled=bwlabeln(...
+    Electrodes_1_and_2_with_noise);
+imagesc(max(Electrodes_1_and_2_with_noise_labeled(:,:,:),[],3))
+Electrodes_1_and_2_with_noise_props=regionprops3...
+    (Electrodes_1_and_2_with_noise_labeled,'Volume');
+% Missing electrodes are labelled 4 and 9
+label_1=4;
+label_2=9;
+Missing_electrode_1=ismember(Electrodes_1_and_2_with_noise_labeled, ...
+    find(Electrodes_1_and_2_with_noise_props.Volume==...
+    Electrodes_1_and_2_with_noise_props{label_1,1}));
+Missing_electrode_2=ismember(Electrodes_1_and_2_with_noise_labeled,...
+    find(Electrodes_1_and_2_with_noise_props.Volume==...
+    Electrodes_1_and_2_with_noise_props{label_2,1}));
+Missing_electrodes_1_and_2=Missing_electrode_1+Missing_electrode_2;
+imagesc(max(Missing_electrodes_1_and_2(:,:,:),[],3))
 %% --------------------------------------------------------------------- %%
-clear Device_matrix_with_noise Matrix_3D_set_1 Matrix_3D_set_2...
-    Set_2_labeled_with_small_noise Set_2_labeled_objects_removed...
-    Volume_values_with_small_noise
-%% ----------------------- Separating electrodes ----------------------- %%
-Electrodes_treshold_1 = 3000;
-Electrodes_treshold_2 = 700;
-Set_2_labeled_separating_electrodes_1_tresh=ismember...
-    (Set_2_labeled_after_opening, find...
-    ([Volume_labeled_open.Volume]<Electrodes_treshold_1));
-Set_2_labeled_electrodes_1=bwlabeln(...
-    Set_2_labeled_separating_electrodes_1_tresh);
-Volume_labeled_electrodes_1=regionprops3(...
-    Set_2_labeled_electrodes_1,'Volume');
-Set_2_labeled_separating_electrodes_2_tresh=ismember...
-    (Set_2_labeled_electrodes_1, find...
-    ([Volume_labeled_electrodes_1.Volume]>Electrodes_treshold_2));
-Set_2_labeled_electrodes_2=bwlabeln(...
-    Set_2_labeled_separating_electrodes_2_tresh);
-figure
-imagesc(max(Set_2_labeled_electrodes_2(:,:,:),[],3))
-jet2=jet;
-jet2(1,:)=0;
+clear Electrodes_1_and_2_with_noise Electrodes_1_and_2_with_noise_labeled...
+    Electrodes_1_and_2_with_noise_props Missing_electrode_1 ...
+    Missing_electrode_2 Volume_electrodes_1_and_2 label_1 label_2
+%% ----------------- Extracting missing electrode 3 -------------------- %%
+% -------- Extracting Volume in which the elctrode are "hiden" --------- %%
+Volume_electrode_3=ismember(wires_labeled,...
+    find(wires_props.Volume==wires_props{6,1}));
+imagesc(max(Volume_electrode_3(:,:,:),[],3))
+strel=strel('line',10, 30);
+Electrode_3_with_noise=imopen(Volume_electrode_3,strel);
+clear strel
+Electrode_3_with_noise_labeled=bwlabeln(...
+    Electrode_3_with_noise);
+imagesc(max(Electrode_3_with_noise_labeled(:,:,:),[],3))
+Electrode_3_with_noise_props=regionprops3...
+    (Electrode_3_with_noise_labeled,'Volume');
+% Missing electrode is labelled 2
+label_3=2;
+Missing_electrode_3=ismember(Electrode_3_with_noise_labeled, ...
+    find(Electrode_3_with_noise_props.Volume==...
+    Electrode_3_with_noise_props{label_3,1}));
+imagesc(max(Missing_electrode_3(:,:,:),[],3))
+%% --------------------------------------------------------------------- %%
+clear Electrode_3_with_noise Electrode_3_with_noise_labeled label_3...
+    Electrode_3_with_noise_props Volume_electrode_3
+%% ----------------- Extracting missing electrode 4 -------------------- %%
+% -------- Extracting Volume in which the elctrode are "hiden" --------- %%
+Volume_electrode_4=ismember(wires_labeled,...
+    find(wires_props.Volume==wires_props{5,1}));
+imagesc(max(Volume_electrode_4(:,:,:),[],3))
+%%
+strel=strel('line',10, 30);
+Electrode_4_with_noise_step_1=imopen(Volume_electrode_4,strel);
+clear strel
+strel=strel('line',10,90);
+Electrode_4_with_noise=imopen(Electrode_4_with_noise_step_1,strel);
+clear strel
+Electrode_4_with_noise_labeled=bwlabeln(...
+    Electrode_4_with_noise);
+imagesc(max(Electrode_4_with_noise_labeled(:,:,:),[],3))
+Electrode_4_with_noise_props=regionprops3...
+    (Electrode_4_with_noise_labeled,'Volume');
+%%
+% Missing electrode is labelled 3
+label_4=3;
+Missing_electrode_4=ismember(Electrode_4_with_noise_labeled, ...
+    find(Electrode_4_with_noise_props.Volume==...
+    Electrode_4_with_noise_props{label_4,1}));
+imagesc(max(Missing_electrode_4(:,:,:),[],3))
+%% --------------------------------------------------------------------- %%
+clear Electrode_4_with_noise Electrode_4_with_noise_labeled label_4...
+    Electrode_4_with_noise_props Electrode_4_with_noise_step_1...
+    Volume_electrode_4
+%% -------------- Extracting missing electrode 5, 6, 7 ----------------- %%
+% -------- Extracting Volume in which the elctrodes are "hiden" -------- %%
+Volume_electrode_5_6_7=ismember(wires_labeled,...
+    find(wires_props.Volume==wires_props{3,1}));
+imagesc(max(Volume_electrode_5_6_7(:,:,:),[],3))
+%%
+strel=strel('line',10, 30);
+Electrode_5_6_7_with_noise_step_1=imopen(Volume_electrode_5_6_7,strel);
+clear strel
+strel=strel('line',10,90);
+Electrode_5_6_7_with_noise=imopen(Electrode_5_6_7_with_noise_step_1,strel);
+clear strel
+Electrode_5_6_7_with_noise_labeled=bwlabeln(...
+    Electrode_5_6_7_with_noise);
+imagesc(max(Electrode_5_6_7_with_noise_labeled(:,:,:),[],3))
+Electrode_5_6_7_with_noise_props=regionprops3...
+    (Electrode_5_6_7_with_noise_labeled,'Volume');
+%%
+% Missing electrodes is labelled 1, 5 and 2
+label_5=1;
+label_6=5;
+label_7=2;
+Missing_electrode_5=ismember(Electrode_5_6_7_with_noise_labeled, ...
+    find(Electrode_5_6_7_with_noise_props.Volume==...
+    Electrode_5_6_7_with_noise_props{label_5,1}));
+Missing_electrode_6=ismember(Electrode_5_6_7_with_noise_labeled, ...
+    find(Electrode_5_6_7_with_noise_props.Volume==...
+    Electrode_5_6_7_with_noise_props{label_6,1}));
+Missing_electrodes_7=ismember(Electrode_5_6_7_with_noise_labeled, ...
+    find(Electrode_5_6_7_with_noise_props.Volume==...
+    Electrode_5_6_7_with_noise_props{label_7,1}));
+Missing_electrodes_5_6_7=Missing_electrode_5+Missing_electrode_6+...
+    Missing_electrodes_7;
+imagesc(max(Missing_electrodes_5_6_7(:,:,:),[],3))
+%% --------------------------------------------------------------------- %%
+clear Electrode_5_6_7_with_noise Electrode_5_6_7_with_noise_labeled...
+    Electrode_5_6_7_with_noise_props Electrode_5_6_7_with_noise_step_1...
+    label_5 label_6 Volume_electrode_5_6_7 Missing_electrode_5...
+    Missing_electrode_6 Missing_electrodes_7 label_7
+%% ------ Adding all the missing electrodes to the elctrodes class ----- %%
+Electrodes_completed=electrodes_opened_labeled+Missing_electrodes_1_and_2...
+    + double(Missing_electrode_3) + double(Missing_electrode_4)...
+    +Missing_electrodes_5_6_7;
+Electrodes_completed_labeled=bwlabeln(Electrodes_completed);
+imagesc(max(Electrodes_completed_labeled(:,:,:),[],3))
 colormap(jet2)
-Volume_values_electrodes=regionprops3...
-    (Set_2_labeled_electrodes_2,'Volume','Centroid');
-%% ------------------------ Separating wires --------------------------- %%
-Wires_treshold=4500;
-Set_2_labeled_separating_wires=ismember...
-    (Set_2_labeled_without_small_noise, find...
-    ([Volume_values_without_small_noise.Volume]>Wires_treshold));
-Set_2_labeled_wires=bwlabeln(Set_2_labeled_separating_wires);
-figure
-imagesc(max(Set_2_labeled_wires(:,:,:),[],3))
-jet2=jet;
-jet2(1,:)=0;
-colormap(jet2)
-Volume_values_wires=regionprops3...
-    (Set_2_labeled_wires,'Volume','Centroid');
+%% --------------------------------------------------------------------- %%
+clear electrodes_opened_labeled electrodes_open_labeled_props...
+    Missing_electrode_3 Missing_electrode_4 Missing_electrodes_1_and_2...
+    Missing_electrodes_5_6_7
+
+%% --------------------------------------------------------------------- %%
 %% ------------------------ Set up - electrodes ------------------------ %%
-Electrodes_matrix=Set_2_labeled_electrodes_1;
+Electrodes_matrix=Electrodes_completed_labeled;
 [X_1,Y_1,Z_1]=size(Electrodes_matrix);
 [X_2D,Y_2D]=meshgrid((1:X_1),(1:Y_1));
 Z_2D=ones(size(X_2D));
@@ -176,32 +246,11 @@ Z_3D=zeros(X_1,Y_1,Z_1);
 for i=1:Z_1
     Z_3D(:,:,i)=Z_2D*i;
 end
-%% ------------------------ Set up - wires ------------------------ %%
-Wires_matrix=Set_2_labeled_wires;
-[X_1w,Y_1w,Z_1w]=size(Electrodes_matrix);
-[X_2Dw,Y_2Dw]=meshgrid((1:X_1w),(1:Y_1w));
-Z_2Dw=ones(size(X_2Dw));
-X_3Dw=repmat(X_2Dw,[1 1 Z_1w]);
-Y_3Dw=repmat(Y_2Dw,[1 1 Z_1w]);
-Z_3Dw=zeros(X_1w,Y_1w,Z_1w);
-for i=1:Z_1w
-    Z_3Dw(:,:,i)=Z_2Dw*i;
-end
-%% --------------------------------------------------------------------- %%
-clear Device_matrix_with_noise Matrix_3D_set_1 Matrix_3D_set_2...
-    Wires_treshold jet2 Electrodes_treshold Set_2_labeled_electrodes...
-    Set_2_labeled_wires Set_2_labeled_with_small_noise...
-    Set_2_labeled_without_small_noise Small_noise_treshold...
-    Set_2_labeled_objects_removed Set_2_labeled_separating_electrodes...
-    Set_2_labeled_separating_wires Volume_values_without_small_noise...
-    Volume_values_wires Volume_values_with_small_noise
 %% --------------- Converting matrix 3D into 3D model ------------------ %%
 figure
-isovalue_2=1;
-patch_e=patch(isosurface(X_3D,Y_3D,Z_3D,Electrodes_matrix,isovalue_2));
-patch_w=patch(isosurface(X_3Dw,Y_3Dw,Z_3Dw,Wires_matrix,isovalue_2));
+isovalue_e=1;
+patch_e=patch(isosurface(X_3D,Y_3D,Z_3D,Electrodes_matrix,isovalue_e));
 set(patch_e,'FaceColor','Magenta','EdgeColor','None')
-set(patch_w,'FaceColor','Green','EdgeColor','None')
 camlight('right')
 lighting gouraud
 material shiny
@@ -209,9 +258,9 @@ view(170,50)
 grid on
 axis([-X_1/4 X_1*1.2 -Y_1/4 Y_1*1.2 -Z_1/4 Z_1*1.2])
 %% ---------------------- Adding text labels --------------------------- %%
-for k=1:size(Volume_values_electrodes,1)
-    text(round(Volume_values_electrodes.Centroid(k,1)),...
-    round(Volume_values_electrodes.Centroid(k,2)),...
-    round(Volume_values_electrodes.Centroid(k,3)),...
-    num2str(k,2),'Color','black','FontSize',10)
+for Volume_electrodes_1_and_2=1:62
+    text(round(Volume_values_electrodes.Centroid(Volume_electrodes_1_and_2,1)),...
+    round(Volume_values_electrodes.Centroid(Volume_electrodes_1_and_2,2)),...
+    round(Volume_values_electrodes.Centroid(Volume_electrodes_1_and_2,3)),...
+    num2str(Volume_electrodes_1_and_2,2),'Color','black','FontSize',10)
 end
